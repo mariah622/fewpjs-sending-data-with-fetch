d6c5a1ef72061cc8cc1c23436cae99b6d0dd400a
mariah622 <mjustine622@gmail.com>
2021-12-20 12:49:13 -0500

    commit tree 0037253e7768e8c370b22628a8edc0e623572823

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+
+
+function submitData(name, email){
+    const formData = {
+        name: name,
+        email: email
+      };
+      
+      const configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+      
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          document.body.innerHTML = object.id
+        })
+
+        .catch(function(error) {
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
+        });
+}
